a372536740b8471c5cb99ad95c3d3b87b99d2665
Kostev Oleg <olkostev@gmail.com>
2026-06-24 16:07:44 +0400

    Add Prisma service module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { validationSchema } from './config/env.validation';
+import { PrismaModule } from 'src/prisma/prisma-module';
 
 @Module({
   imports: [
+    PrismaModule,
     ConfigModule.forRoot({
       isGlobal: true,
       validationSchema,
