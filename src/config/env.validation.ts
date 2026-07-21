978f18596890a8c9b1c0d66f945e60c37d4a019b
Kostev Oleg <olkostev@gmail.com>
2026-07-22 00:35:49 +0400

    Add object storage setup

--- src/config/env.validation.ts
+++ src/config/env.validation.ts
@@ -17,4 +17,10 @@ export const validationSchema = Joi.object({
   JWT_SECRET: Joi.string().required(),
   JWT_ACCESS_TOKEN_EXPIRES_IN: Joi.string().default('15m'),
   JWT_REFRESH_TOKEN_EXPIRES_IN: Joi.string().default('30d'),
+  S3_ENDPOINT: Joi.string().uri().required(),
+  S3_REGION: Joi.string().required(),
+  S3_BUCKET: Joi.string().required(),
+  S3_ACCESS_KEY: Joi.string().required(),
+  S3_SECRET_KEY: Joi.string().required(),
+  S3_FORCE_PATH_STYLE: Joi.boolean().default(true),
 });
